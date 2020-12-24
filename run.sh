8cbb8548036669291f13461962cd8049f512dae2
eddie <aigagror@gmail.com>
2020-12-24 15:39:39 -0600

    1 epoch

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 echo "Hello CHTC from Job $1 running on `hostname`"
-python main.py --bsz=1024 --epochs=10 --method=supcon --lr=1e-3
+python main.py --bsz=1024 --epochs=1 --method=supcon --lr=1e-3
