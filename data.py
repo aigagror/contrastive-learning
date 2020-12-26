9ecebf45f641d9080c86fab481d910daed0bafe6
eddie <aigagror@gmail.com>
2020-12-25 18:42:02 -0600

    tfrecord on cifar10train

--- data.py
+++ data.py
@@ -1,7 +1,7 @@
 import tensorflow as tf
+import tensorflow_datasets as tfds
 from tensorflow.keras import layers, datasets
 from tensorflow.python.data import AUTOTUNE
-import tensorflow_datasets as tfds
 
 
 class Augment(layers.Layer):
@@ -49,13 +49,76 @@ class Augment(layers.Layer):
         return image
 
 
+def _bytes_feature(value):
+    """Returns a bytes_list from a string / byte."""
+    if isinstance(value, type(tf.constant(0))):
+        value = value.numpy()  # BytesList won't unpack a string from an EagerTensor.
+    return tf.train.Feature(bytes_list=tf.train.BytesList(value=[value]))
+
+
+def _float_feature(value):
+    """Returns a float_list from a float / double."""
+    return tf.train.Feature(float_list=tf.train.FloatList(value=[value]))
+
+
+def _int64_feature(value):
+    """Returns an int64_list from a bool / enum / int / uint."""
+    return tf.train.Feature(int64_list=tf.train.Int64List(value=[value]))
+
+
+feature_description = {
+    'image': tf.io.FixedLenFeature([], tf.float32, default_value=0.0),
+    'label': tf.io.FixedLenFeature([], tf.int64, default_value=0),
+}
+
+
+def serialize_example(img, label):
+    """
+    Creates a tf.train.Example message ready to be written to a file.
+    """
+    # Create a dictionary mapping the feature name to the tf.train.Example-compatible
+    # data type.
+    feature = {
+        'image': _int64_feature(img),
+        'label': _int64_feature(label)
+    }
+
+    # Create a Features message using tf.train.Example.
+
+    example_proto = tf.train.Example(features=tf.train.Features(feature=feature))
+    return example_proto.SerializeToString()
+
+
+def tf_serialize_example(img, label):
+    tf_string = tf.py_function(
+        serialize_example,
+        (img, label),  # pass these args to the above function.
+        tf.string)  # the return type is `tf.string`.
+    return tf.reshape(tf_string, ())  # The result is a scalar
+
+
 def load_datasets(args, strategy):
     if args.data == 'cifar10':
         imsize = 32
         (x_train, y_train), (x_test, y_test) = datasets.cifar10.load_data()
         ds_train = tf.data.Dataset.from_tensor_slices((x_train, y_train.flatten())).cache()
         ds_test = tf.data.Dataset.from_tensor_slices((x_test, y_test.flatten())).cache()
-    elif args.data =='imagenet':
+
+        serialized_ds_train = ds_train.map(tf_serialize_example)
+
+        filename = 'cifar10-train.tfrecord'
+        writer = tf.data.experimental.TFRecordWriter(filename)
+        writer.write(serialized_ds_train)
+
+        raw_dataset = tf.data.TFRecordDataset([filename])
+        def _parse_function(example_proto):
+            # Parse the input `tf.train.Example` proto using the dictionary above.
+            example = tf.io.parse_single_example(example_proto, feature_description)
+            return example['image'], example['label']
+
+        ds_train = raw_dataset.map(_parse_function)
+
+    elif args.data == 'imagenet':
         imsize = 224
         ds_train = tfds.folder_dataset.ImageFolder(args.imagenet_train).as_dataset(shuffle_files=True)
         ds_test = tfds.folder_dataset.ImageFolder(args.imagenet_val).as_dataset(shuffle_files=True)
