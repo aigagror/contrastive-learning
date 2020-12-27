849072127c55816424898d1da01c2d043f98bee0
eddie <aigagror@gmail.com>
2020-12-26 20:26:47 -0600

    make outdir if necessary

--- main.py
+++ main.py
@@ -36,6 +36,9 @@ parser.add_argument('--out', type=str, default='out/')
 
 
 def setup(args):
+    if not os.path.exists(args.out):
+        os.makedirs(args.out)
+
     if args.tpu:
         resolver = tf.distribute.cluster_resolver.TPUClusterResolver()
         tf.config.experimental_connect_to_cluster(resolver)
