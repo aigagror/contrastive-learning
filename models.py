75816ec13aae11a2be2083a968319c1e1fb7b64b
eddie <aigagror@gmail.com>
2021-01-01 19:01:07 -0600

    print debug

--- models.py
+++ models.py
@@ -71,7 +71,6 @@ class ContrastModel(keras.Model):
                 pred_logits, _ = self(imgs1)
 
             # Classifer cross entropy
-            print(pred_logits)
             tf.print(tf.reduce_min(labels), tf.reduce_max(labels))
             ce_loss = losses.sparse_categorical_crossentropy(labels, pred_logits, from_logits=True)
             ce_loss = tf.nn.compute_average_loss(ce_loss, global_batch_size=bsz)
