9bf4634151d0b2bc34c8fd1b7cd98cea5804ddb1
eddie <aigagror@gmail.com>
2021-01-10 15:12:50 -0600

    changed default out directory

--- utils.py
+++ utils.py
@@ -25,7 +25,7 @@ parser.add_argument('--policy', choices=['mixed_bfloat16', 'float32'], default='
 # Other
 parser.add_argument('--load', action='store_true')
 parser.add_argument('--tsne', action='store_true')
-parser.add_argument('--out', type=str, default='./')
+parser.add_argument('--out', type=str, default='out/')
 
 
 def setup(args):
