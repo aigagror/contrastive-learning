443ad45e9db13d825fa9e1c044f956c6951863d5
eddie <aigagror@gmail.com>
2021-01-10 15:44:47 -0600

    debug

--- models.py
+++ models.py
@@ -93,26 +93,26 @@ class ContrastModel(keras.Model):
         sims = tf.matmul(feats1, tf.transpose(feats2))
 
         loss = 0
-        # if partial:
-        #     # Cross entropy on instance similarities
-        #     inst_loss = losses.categorical_crossentropy(inst_mask, sims * 10, from_logits=True)
-        #
-        #     # Partial cross entropy on class similarities
-        #     pos_mask = tf.maximum(inst_mask, class_mask)
-        #     neg_mask = 1 - pos_mask
-        #
-        #     exp = tf.math.exp(sims * 10)
-        #     neg_sum_exp = tf.math.reduce_sum(exp * neg_mask, axis=1, keepdims=True)
-        #     log_prob = sims - tf.math.log(neg_sum_exp + exp)
-        #
-        #     # Class positive pairs log prob (contains instance positive pairs too)
-        #     class_log_prob = class_mask * log_prob
-        #     class_log_prob = tf.math.reduce_sum(class_log_prob / class_sum, axis=1)
-        #     class_loss = -class_log_prob
-        #
-        #     # Combine instance loss and class loss
-        #     loss = inst_loss + class_loss
-        # else:
-        #     # Cross entropy on everything
-        #     loss = losses.categorical_crossentropy(class_mask / class_sum, sims * 10, from_logits=True)
+        if partial:
+            # Cross entropy on instance similarities
+            inst_loss = losses.categorical_crossentropy(inst_mask, sims * 10, from_logits=True)
+
+            # Partial cross entropy on class similarities
+            pos_mask = tf.maximum(inst_mask, class_mask)
+            neg_mask = 1 - pos_mask
+
+            exp = tf.math.exp(sims * 10)
+            neg_sum_exp = tf.math.reduce_sum(exp * neg_mask, axis=1, keepdims=True)
+            log_prob = sims - tf.math.log(neg_sum_exp + exp)
+
+            # Class positive pairs log prob (contains instance positive pairs too)
+            class_log_prob = class_mask * log_prob
+            class_log_prob = tf.math.reduce_sum(class_log_prob / class_sum, axis=1)
+            class_loss = -class_log_prob
+
+            # Combine instance loss and class loss
+            loss = inst_loss + class_loss
+        else:
+            # Cross entropy on everything
+            loss = losses.categorical_crossentropy(class_mask / class_sum, sims * 10, from_logits=True)
         return loss
