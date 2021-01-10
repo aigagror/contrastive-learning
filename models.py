d439cb2d6081bbe7fe5d4e459f116e17c960c098
eddie <aigagror@gmail.com>
2021-01-10 16:03:17 -0600

    debug

--- models.py
+++ models.py
@@ -113,5 +113,6 @@ class ContrastModel(keras.Model):
         #     loss = inst_loss + class_loss
         # else:
         #     # Cross entropy on everything
-        loss = losses.categorical_crossentropy(class_mask / class_sum, sims * 10, from_logits=True)
+        # loss = losses.categorical_crossentropy(class_mask / class_sum, sims * 10, from_logits=True)
+        loss = 0
         return loss
