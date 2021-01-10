9cb03d81887947a3e9ff7276d758beb81656ca30
eddie <aigagror@gmail.com>
2021-01-10 15:08:29 -0600

    only one out argument

--- utils.py
+++ utils.py
@@ -25,8 +25,7 @@ parser.add_argument('--policy', choices=['mixed_bfloat16', 'float32'], default='
 # Other
 parser.add_argument('--load', action='store_true')
 parser.add_argument('--tsne', action='store_true')
-parser.add_argument('--metrics-out', type=str, default='out/')
-parser.add_argument('--model-out', type=str, default='out/model')
+parser.add_argument('--out', type=str, default='./')
 
 
 def setup(args):
