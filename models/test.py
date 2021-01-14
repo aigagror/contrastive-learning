6eb96e8592f25ffbf07605a9d9994091f0c39130
eddie <aigagror@gmail.com>
2021-01-14 13:34:38 -0600

    refactor

--- models/test.py
+++ models/test.py
@@ -4,7 +4,7 @@ from models import small_resnet_v2
 
 
 class TestSmallResnet(unittest.TestCase):
-    def test_output_shape(self):
+    def test_resnet50v2_output_shape(self):
         small_resnet = small_resnet_v2.SmallResNet50V2(include_top=False, input_shape=[32, 32, 3])
         out_shape = small_resnet.output_shape
         self.assertEqual(out_shape, (None, 4, 4, 512))
