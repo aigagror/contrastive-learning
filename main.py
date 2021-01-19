decaf4862fcd9df9027e41c0ed56e993069aee8a
eddie <aigagror@gmail.com>
2021-01-19 13:15:14 -0600

    bug fix

--- main.py
+++ main.py
@@ -12,18 +12,18 @@ def run(args):
     strategy = utils.setup(args)
 
     # Data
-    ds_train, ds_val, info = data.load_datasets(args)
+    ds_train, ds_val, ds_info = data.load_datasets(args)
     plots.plot_img_samples(args, ds_train, ds_val)
 
     # Model and optimizer
     with strategy.scope():
-        model = models.ContrastModel(args, info['nclass'], info['input_shape'])
+        model = models.ContrastModel(args, ds_info['nclass'], ds_info['input_shape'])
         model.compile(optimizers.SGD(args.lr, momentum=0.9), steps_per_execution=args.steps_exec)
         if args.debug:
             model.cnn.summary()
 
     # Train
-    train(args, model, ds_train, ds_val)
+    train(args, model, ds_train, ds_val, ds_info)
 
     # Plot
     plots.plot_hist_sims(args, strategy, model, ds_val)
