566abf4f7c77943d18bb91f243fa76477d962597
eddie <aigagror@gmail.com>
2021-01-20 14:40:55 -0600

    optional lr decay

--- training.py
+++ training.py
@@ -13,28 +13,34 @@ def train(args, model, ds_train, ds_val, ds_info):
             shutil.rmtree(args.out)
             os.mkdir(args.out)
 
-    def scheduler(epoch, lr):
-        if epoch in args.lr_decays:
-            return lr * 0.1
-        else:
-            return lr
+    cbks = [
+        callbacks.TensorBoard(log_dir, histogram_freq=1, update_freq=args.update_freq),
+        callbacks.ModelCheckpoint(os.path.join(args.out, 'model'), save_weights_only=True)
+    ]
+
+    # Learning rate schedule
+    if args.lr_decays:
+        def scheduler(epoch, lr):
+            if epoch in args.lr_decays:
+                return lr * 0.1
+            else:
+                return lr
+
+        cbks.append(callbacks.LearningRateScheduler(scheduler, verbose=1))
 
     try:
-        cbks = [
-            callbacks.TensorBoard(log_dir, histogram_freq=1, update_freq=args.update_freq),
-            callbacks.LearningRateScheduler(scheduler, verbose=1),
-            callbacks.ModelCheckpoint(os.path.join(args.out, 'model'), save_weights_only=True)
-        ]
 
         train_steps, val_steps = args.train_steps, args.val_steps
         if args.steps_exec is not None:
             ds_train, ds_val = ds_train.repeat(), ds_val.repeat()
             if args.train_steps is None:
                 train_steps = ds_info['train_size'] // args.bsz
-                print(f'steps per execution set and train_steps not specified. setting it to train_size // bsz = {train_steps}')
+                print(
+                    f'steps per execution set and train_steps not specified. setting it to train_size // bsz = {train_steps}')
             if args.val_steps is None:
                 val_steps = ds_info['val_size'] // args.bsz
-                print(f'steps per execution set and val_steps not specified. setting it to val_size // bsz = {val_steps}')
+                print(
+                    f'steps per execution set and val_steps not specified. setting it to val_size // bsz = {val_steps}')
 
         model.fit(ds_train, validation_data=ds_val, validation_steps=val_steps,
                   initial_epoch=args.init_epoch, epochs=args.epochs, steps_per_epoch=train_steps,
