10ce56ee37aacb80f37086c39b847bb781fb65bb
eddie <aigagror@gmail.com>
2021-01-21 13:41:09 -0600

    include inst sims in partial sup con

--- models/custom_losses.py
+++ models/custom_losses.py
@@ -51,10 +51,7 @@ class PartialSupCon(losses.Loss):
         # Masks
         inst_mask = tf.eye(bsz, ncol, dtype=dtype)
         class_mask = tf.cast(y_true, dtype)
-
-        diag_part = tf.linalg.diag_part(class_mask)
-        non_inst_class_mask = tf.linalg.set_diag(class_mask, tf.zeros_like(diag_part))
-        non_inst_class_sum = tf.math.reduce_sum(non_inst_class_mask, axis=1, keepdims=True)
+        class_sum = tf.math.reduce_sum(class_mask, axis=1, keepdims=True)
 
         # Similarities
         sims = y_pred * 10
@@ -68,8 +65,8 @@ class PartialSupCon(losses.Loss):
         partial_log_prob = sims - tf.math.log(neg_sum_exp + exp)
 
         # Class positive pairs log prob (contains instance positive pairs too)
-        class_partial_log_prob = non_inst_class_mask * partial_log_prob
-        class_partial_log_prob = tf.math.reduce_sum(class_partial_log_prob / (non_inst_class_sum + 1e-3), axis=1)
+        class_partial_log_prob = class_mask * partial_log_prob
+        class_partial_log_prob = tf.math.reduce_sum(class_partial_log_prob / (class_sum + 1e-3), axis=1)
         partial_supcon_loss = -class_partial_log_prob
 
         return partial_supcon_loss
