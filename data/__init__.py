2fd96e0a74310b718a9bd66b3087d26c2e9854b5
eddie <aigagror@gmail.com>
2021-01-21 22:38:06 -0600

    repeat datasets if specified steps

--- data/__init__.py
+++ data/__init__.py
@@ -38,10 +38,10 @@ def load_datasets(args):
     ds_train = ds_train.prefetch(tf.data.AUTOTUNE)
     ds_val = ds_val.prefetch(tf.data.AUTOTUNE)
 
-    if args.train_steps:
+    if args.train_steps or args.steps_exec:
         ds_train = ds_train.repeat()
         print('repeating training dataset')
-    if args.val_steps:
+    if args.val_steps or args.steps_exec:
         ds_val = ds_val.repeat()
         print('repeating validation dataset')
 
