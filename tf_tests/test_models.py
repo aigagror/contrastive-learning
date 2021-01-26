4cf54e19064e5773b61ec132e3b319c59331d1b2
eddie <aigagror@gmail.com>
2021-01-25 19:00:28 -0600

    using SGD with weight decay instead of adding l2 reg

--- tf_tests/test_models.py
+++ tf_tests/test_models.py
@@ -1,6 +1,5 @@
 import unittest
 
-import numpy as np
 import tensorflow as tf
 from tensorflow import keras
 
@@ -16,26 +15,6 @@ class TestModel(unittest.TestCase):
         small_resnet = small_resnet_v2.SmallResNet50V2(include_top=False, input_shape=[32, 32, 3])
         out_shape = small_resnet.output_shape
         self.assertEqual(out_shape, (None, 4, 4, 2048))
-
-    def test_l2_reg(self):
-        args = '--data=cifar10 --model=small-resnet50v2 --l2-reg=1e-3 ' \
-               '--bsz=8 --lr=1e-3 --loss=ce '
-        args = utils.parser.parse_args(args.split())
-        utils.setup(args)
-
-        model = models.make_model(args, nclass=10, input_shape=[32, 32, 3])
-        model = training.compile_model(args, model)
-        count = 0
-        for module in model.submodules:
-            for attr in ['kernel_regularizer', 'bias_regularizer']:
-                if hasattr(module, attr):
-                    count += 1
-                    self.assertEqual(getattr(module, attr).l2, np.array(1e-3, dtype=np.float32),
-                                     getattr(module, attr).l2)
-
-        # Assert at least 40 modules were regularized
-        self.assertGreaterEqual(count, 40)
-        self.assertGreaterEqual(len(model.losses), 40)
 
     def test_no_grad_ce(self):
         args = '--data=cifar10 --model=small-resnet50v2 ' \
