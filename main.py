c59a3b2f34887b504aaac6533345a51cdab60852
eddie <aigagror@gmail.com>
2021-01-26 21:23:44 -0600

    print reg losses

--- main.py
+++ main.py
@@ -34,8 +34,7 @@ def run(args):
             training.compile_model(args, model)
             print('starting with new model')
 
-        if len(model.losses) <= 0:
-            print('WARNING: No regularization in this model')
+        print(f'{len(model.losses)} regularization losses in this model')
 
     if args.debug:
         model.summary()
