277bdc35843a0962225753b2e7d8b3aa7fcf98a7
eddie <aigagror@gmail.com>
2021-02-03 14:53:38 -0600

    bug fix

--- models/custom_layers.py
+++ models/custom_layers.py
@@ -158,7 +158,7 @@ class SpectralNormalization(tf.keras.layers.Wrapper):
 
             sigma = tf.matmul(tf.matmul(v, w), u, transpose_b=True)
 
-            self.w.assign(self.w / tf.cast(sigma, self.w.dtype))
+            self.w.assign(self.w / tf.cast(sigma, self.compute_dtype))
             self.u.assign(u)
 
     def get_config(self):
