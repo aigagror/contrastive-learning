398e971f03bcd392b4489e723c12e15b7a8519d1
eddie <aigagror@gmail.com>
2021-02-09 16:27:46 -0600

    another debug print

--- tf_tests/test_plots.py
+++ tf_tests/test_plots.py
@@ -1,7 +1,6 @@
 import logging
 import unittest
 
-import tensorflow as tf
 import tensorflow_datasets as tfds
 
 import data
@@ -14,10 +13,10 @@ class TestPlots(unittest.TestCase):
     def basic_usage(self):
         args = '--data-id=mnist --backbone=affine ' \
                '--bsz=8 --lr=1e-3 ' \
-               '--loss=partial-supcon '
+               '--loss=partial-supcon --multi-cpu'
+
         args = utils.parser.parse_args(args.split())
-        utils.setup(args)
-        strategy = tf.distribute.MirroredStrategy(['CPU:0', 'CPU:1'])
+        strategy = utils.setup(args)
 
         _, ds_info = tfds.load(args.data_id, try_gcs=True, data_dir='gs://aigagror/datasets', with_info=True)
         train_augment_config, val_augment_config = utils.load_augment_configs(args)
@@ -30,6 +29,11 @@ class TestPlots(unittest.TestCase):
         return args, strategy, model, ds_train
 
     def test_hist(self):
+        args, strategy, model, ds_val = self.basic_usage()
+        plots.plot_hist_sims(args, strategy, model, ds_val, max_iter=1)
+        logging.info(f'hist saved to {args.out}')
+
+    def test_distributed_hist(self):
         args, strategy, model, ds_val = self.basic_usage()
         plots.plot_hist_sims(args, strategy, model, ds_val, max_iter=1)
         logging.info(f'hist saved to {args.out}')
